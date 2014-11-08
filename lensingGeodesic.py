cc5bc9f343d695ae18830c0644259b5d6eee1498
Orion Sauter <osauter@umich.edu>
2014-11-07 20:10:19 -0500

    Starting over – First step of generating metric is solving stellar structure diff eqs.

--- lensingGeodesic.py
+++ lensingGeodesic.py
@@ -1,75 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 
 G = 1.0
+pi = np.pi
+rho = 0.1
 
-def calcPotential(masses):
-    m = np.size(masses,0)
-    n = np.size(masses,1)
-    inds = np.mgrid[0:m,0:n]
-    phi = np.zeros([m,n])
-    for i in range(m):
-        for j in range(n):
-            g = -G*masses[i,j]
-            r = np.sqrt((inds[0]-i)**2+(inds[1]-j)**2)
-            r[i,j] = 1
-            dphi = g/r
-            dphi[i,j] = 0
-            phi = phi + dphi
-    return phi
+def diffEq(mp,r):
+    dpdr = -G**2 * (rho + mp[1]) * (mp[0] + 4.0*pi*r**3*mp[1])/(r*(r-2*G*mp[0]))
+    dmdr = 4.0*pi*r**2*rho
+    return np.array([dmdr,dpdr])
+    
 
-def getMetric(phi):
-    m = np.size(phi,0)
-    n = np.size(phi,1)
-    gddDiag = np.zeros([m,n,4])
-    gddDiag[:,:,0] = -1 - 2.0*phi[:,:]
-    gddDiag[:,:,1] = 1 - 2.0*phi[:,:]
-    gddDiag[:,:,2] = gddDiag[:,:,1]
-    gddDiag[:,:,3] = gddDiag[:,:,1]
-    return gddDiag
-
-def getChristof(gddDiagIn,x):
-    gddDiag = np.zeros_like(gddDiagIn)
-    if (x[2] > 0):
-        gddDiag[:,:,0] = ( 1.0 + gddDiagIn[:,:,0])/x[2] - 1.0
-        gddDiag[:,:,1] = (-1.0 + gddDiagIn[:,:,1])/x[2] + 1.0
-        gddDiag[:,:,2] = (-1.0 + gddDiagIn[:,:,2])/x[2] + 1.0
-        gddDiag[:,:,3] = (-1.0 + gddDiagIn[:,:,3])/x[2] + 1.0
-    else:
-        gddDiag[:] = gddDiagIn
-    christof = np.zeros([4,4,4])
-    m = np.size(gddDiag,0)
-    n = np.size(gddDiag,1)
-    dg = np.zeros([m,n,4,4],dtype=float)
-    temp = np.gradient(gddDiag[:,:,0])
-    dg[:,:,0,1] = temp[0]
-    dg[:,:,0,2] = temp[1]
-    temp = np.gradient(gddDiag[:,:,1])
-    dg[:,:,1,1] = temp[0]
-    dg[:,:,1,2] = temp[1]
-    temp = np.gradient(gddDiag[:,:,2])
-    dg[:,:,2,1] = temp[0]
-    dg[:,:,2,2] = temp[1]
-    temp = np.gradient(gddDiag[:,:,3])
-    dg[:,:,3,1] = temp[0]
-    dg[:,:,3,2] = temp[1]
-    #dg[:,:,:,3] = (-1.0 + gddDiag[:,:,:])/x[2]
-    for i in range(4):
-        for j in range(4):
-            christof[i,j,i] = 0.5 * (1.0/gddDiag[x[0],x[1],i]) * dg[x[0],x[1],i,j]
-            christof[i,i,j] = christof[i,j,i]
-            if (i != j):
-                christof[i,j,j] = -christof[i,j,i]
-    return christof
-
-m = 9
-n = 9
-inds = np.mgrid[0:m,0:n]
-masses = 1.0/np.sqrt((inds[0]-0.5*m)**2 + (inds[1]-0.5*n)**2)
-phi = calcPotential(masses)
-gddDiag = getMetric(phi)
-christof = getChristof(gddDiag,[0,3,1])
-print christof
-
-#plt.matshow(phi)
-#plt.show()
+mp0 = np.array([0.0,0.1])
+r = np.arange(1.0,2.0,0.01)
+out = odeint(diffEq,mp0,r)
+out = np.array(out)
+neg = next(i for i in range(np.size(out,0)) if out[i,1] < 0)
+out = out[0:(neg-1),:]
+r = r[0:(neg-1)]
+plt.plot(r,out[:,1])
+plt.show()
