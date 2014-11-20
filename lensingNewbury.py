Fixed error in AlphaTwiddle calculation and vectorized calculation.

lensingNewbury.py
@@ -10,16 +10,16 @@ Dds = Ds - Dd
 xi0 = 4.8481368111e-6 * Dd
 
 def calcAlphaTw(xi,masses):
-    n = np.size(masses,0)
-    m = np.size(masses,1)
+    m = np.size(masses,0)
+    n = np.size(masses,1)
     a = np.zeros(2)
-    for i in range(n):
-        for j in range(m):
-            xip = np.array([i,j],dtype=float)
-            rsq = np.sum(np.square(xi - xip))
-            if (rsq < 0.5):
-                continue
-            a = a + 4.0*G*masses[i,j]*(xi - xip)/(c**2*rsq)
+    xi2 = np.array([xi[0]*np.ones([m,n]),xi[1]*np.ones([m,n])],dtype=float)
+    xip = xi0 * np.array(np.mgrid[0:m,0:n],dtype=float)
+    rsq = np.square(xi2[0,:,:] - xip[0,:,:]) + np.square(xi2[1,:,:] - xip[1,:,:])
+    rsq[rsq < 2.0] = 2.0
+    masses2 = np.array([masses, masses])
+    rsq2 = np.array([rsq,rsq])
+    a = np.sum(np.sum(4.0*G*masses2*(xi2 - xip)/(c**2*rsq2),axis=1),axis=1)
     return a
 
 def calcAlpha(x,masses):
@@ -47,8 +47,8 @@ def magnif(x,dx,masses):
     return mag
 
 def fuzzyIndex(y,m,n):
-    dist = np.zeros([m,n])
-    if (any(y < 0) or y[0] >= m or y[1] >= n):
+    dist = np.zeros([m,n],dtype=float)
+    if (any(np.floor(y) < 0) or np.ceil(y[0]) >= m or np.ceil(y[1]) >= n):
         return dist
     y1 = [np.floor(y[0]),np.floor(y[1])]
     y2 = [np.floor(y[0]),np.ceil(y[1])]
@@ -59,22 +59,22 @@ def fuzzyIndex(y,m,n):
     dist[y2[0],y2[1]] = np.sqrt(np.sum((y - y2)**2))
     dist[y3[0],y3[1]] = np.sqrt(np.sum((y - y3)**2))
     dist[y4[0],y4[1]] = np.sqrt(np.sum((y - y4)**2))
+    dist = dist/np.sum(dist)
     return dist
 
-source = imread('test.png')
-#source = imread('liu.png')
+#source = imread('test.png')
+source = imread('orion.png')
 plt.imshow(source)
 m = np.size(source,0)
 n = np.size(source,1)
 image = np.zeros([m,n,3])
-inds = np.mgrid[0:m,0:n]
-masses = 1.0/np.sqrt((inds[0]-0.5*m)**2 + (inds[1]-0.5*n)**2)
-#masses = 0.0001*np.ones([m,n])
-#plt.matshow(masses)
-#plt.show()
+inds = np.array(np.mgrid[0:m,0:n],dtype=float)
+r = np.sqrt((inds[0]-0.5*float(m-1))**2 + (inds[1]-0.5*float(n-1))**2) + 1
+masses = 100.0/r
+plt.matshow(masses)
 for i in range(m):
+    print str(i*n)+'/'+str(m*n)
     for j in range(n):
-        #print str(j+i*m)+'/'+str(m*n)
         x = np.array([i,j])
         a = calcAlpha(x,masses)
         y = x - a